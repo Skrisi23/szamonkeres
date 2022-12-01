9b38e6beb87862fc4bcf6dfb1e80b78eb61615f7
Somogyi Kristóf <Somogyik@KKSZKI.LOCAL>
2022-12-01 13:25:26 +0100

    2feladatkesz

--- python szamonkeres/1_feladat.py
+++ python szamonkeres/1_feladat.py
@@ -1,4 +1,12 @@
+#1feladat
+"""
 nev1 = input("Kérem a Vezetékneved:  ")
 nev2 = input("Kérem a Kereszneved:  ")
 print("Üdvözöllek")
 print(nev1, nev2)
+"""
+#2feladat
+
+szam = int(input("Irj be egy számot: "))
+print("A számot megelőző :", (szam - 1))
+print("A számmot rákövetkező :", (szam + 1))
